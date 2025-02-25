e0af6f5d13f04cdb87d8913c771bb874a5c71242
Hurriat Ali <hurriat.ali@spotmyenergy.de>
2025-02-25 16:20:42 +0100

    Chapter 7

---
+++ estate/models/estate_property.py
@@ -0,0 +1,40 @@
+from odoo import models, fields
+from datetime import timedelta, date
+
+class EstateProperty(models.Model):
+    _name = "estate.property"
+    _description = "Real Estate Property"
+
+    name = fields.Char(required=True)
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date(copy=False, default=lambda self: date.today() + timedelta(days=90))
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float()
+    bedrooms = fields.Integer(default=2)
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    property_type_id = fields.Many2one("estate.property.type", string="Property Type")
+    buyer_id = fields.Many2one("res.partner", string="Buyer", copy=False)
+    offer_ids = fields.One2many("estate.property.offer", "property_id", string="Offers")
+    salesperson_id = fields.Many2one("res.users", string="Salesperson", default=lambda self: self.env.user)
+    garden_orientation = fields.Selection(
+        [('north', 'North'),
+         ('south', 'South'),
+         ('east', 'East'),
+         ('west', 'West')]
+    )
+    active = fields.Boolean(default=True)
+    state = fields.Selection(
+        [('new', 'New'),
+         ('offer_received', 'Offer Received'),
+         ('offer_accepted', 'Offer Accepted'),
+         ('sold', 'Sold'),
+         ('cancelled', 'Cancelled')],
+        required=True, copy=False, default='new'
+    )
+
+    selling_price = fields.Float(readonly=True)
